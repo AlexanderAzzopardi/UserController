b4f3040131a8a1d2ee2a1f721620fff0737a55a4
Alexander Azzopardi <87972109+AlexanderAzzopardi@users.noreply.github.com>
2021-08-16 12:23:47 +0100

    Add files via upload

---
+++ sample.microservice.useraccount/Models/Account.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace sample.microservice.useraccount.Models
+{
+    public class Account
+    {
+        public Guid Id {get; set;}
+        public string Name {get; set;}
+    }
+}
